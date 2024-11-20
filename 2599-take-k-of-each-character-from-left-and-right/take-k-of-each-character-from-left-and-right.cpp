90458b8b50c408883f046e331b946fa957e4ac39
Nkosinathi Michael Sibanda <23mh1a05h9@acoe.edu.in>
2024-11-20 18:50:46 +0530

    Time: 19 ms (49.76%) | Memory: 12 MB (92.24%) - LeetSync

--- 2599-take-k-of-each-character-from-left-and-right/take-k-of-each-character-from-left-and-right.cpp
+++ 2599-take-k-of-each-character-from-left-and-right/take-k-of-each-character-from-left-and-right.cpp
@@ -2,34 +2,27 @@ class Solution {
 public:
     int takeCharacters(string s, int k) {
         int n =s.length();
-        int freqa=0, freqb = 0, freqc = 0, i = 0, j = n-1;
+        int freq[3] = {0}, i = 0, j = n-1;
         for(i=0;i<n;i++){
-            if(freqa >= k && freqb >= k && freqc >= k){
+            if(freq[0] >= k && freq[1] >= k && freq[2] >= k){
                 break;
             }
-            if(s[i] == 'a') freqa++;
-            else if(s[i] == 'b') freqb++;
-            else if(s[i] == 'c') freqc++;
+            freq[s[i]-'a']++;
         }
-        if(freqa < k || freqb < k || freqc < k){
+        if(freq[0] < k || freq[1] < k || freq[2] < k){
             return -1;
         }
-        int mini = i;
-        i--;
+        int mini = i--;
         while(j>=0 && i>=0 && i<=j){
-            if(s[i] == 'a') freqa--;
-            else if(s[i] == 'b') freqb--;
-            else if(s[i] == 'c') freqc--;
-            if(freqa < k || freqb < k || freqc < k){
-                if(s[j] == 'a') freqa++;
-                else if(s[j] == 'b') freqb++;
-                else if(s[j] == 'c') freqc++;
+            freq[s[i]-'a']--;
+            i--;
+            if(freq[0] < k || freq[1] < k || freq[2] < k){
+                freq[s[j]-'a']++;
                 j--;
             }
-            if(freqa >= k && freqb >= k && freqc >= k){
-                mini = min(mini, n-(j-i)-1);
+            if(freq[0] >= k && freq[1] >= k && freq[2] >= k){
+                mini = min(mini, n-(j-i));
             }
-            i--;
         }
         return mini;
     }
