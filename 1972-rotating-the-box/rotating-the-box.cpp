f0754000602d274eb7a3b827a98f06b554ccff10
Nkosinathi Michael Sibanda <23mh1a05h9@acoe.edu.in>
2024-11-23 19:12:49 +0530

    Time: 181 ms (86.67%) | Memory: 56.4 MB (63.41%) - LeetSync

--- 1972-rotating-the-box/rotating-the-box.cpp
+++ 1972-rotating-the-box/rotating-the-box.cpp
@@ -2,29 +2,24 @@ class Solution {
 public:
     vector<vector<char>> rotateTheBox(vector<vector<char>>& box) {
         int n = box.size(), m = box[0].size();
-        for(int i =0;i<n;i++){
+        vector<vector<char>> res(m, vector<char> (n, '.'));
+        for(int i=0;i<n;i++){
             int count = 0;
             for(int j=0;j<m;j++){
                 if(box[i][j] == '#'){
                     count++;
-                    box[i][j] = '.';
                 }
                 else if(box[i][j] == '*'){
-                    while(count>0){
-                        box[i][j-count]='#';
+                    res[j][n-1-i] = '*';
+                    while(count){
+                        res[j-count][n-i-1]='#';
                         count--;
                     }
                 }
             }
-            while(count>0){
-                box[i][m-count] = '#';
+            while(count){
+                res[m-count][n-1-i] = '#';
                 count--;
-            }
-        }
-        vector<vector<char>> res(m, vector<char> (n));
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                res[j][i] = box[n-i-1][j];
             }
         }
         return res;
