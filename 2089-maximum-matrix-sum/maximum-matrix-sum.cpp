8ca3cea5635022f243024835d636b8d464fa4abd
Nkosinathi Michael Sibanda <23mh1a05h9@acoe.edu.in>
2024-11-24 15:34:41 +0530

    Time: 3 ms (42.79%) | Memory: 38.4 MB (30.74%) - LeetSync

--- 2089-maximum-matrix-sum/maximum-matrix-sum.cpp
+++ 2089-maximum-matrix-sum/maximum-matrix-sum.cpp
@@ -1,19 +1,18 @@
 class Solution {
 public:
     long long maxMatrixSum(vector<vector<int>>& matrix) {
-        int mini = INT_MAX;
-        int neg = 0;
+        int mini = INT_MAX, neg = 0, n = matrix.size();
         long long maxi = 0;
-        for(auto row : matrix){
-            for(int i=0;i<row.size();i++){
-                mini = min(mini, abs(row[i]));
-                if(row[i] < 0) neg++;
-                maxi += abs(row[i]);
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < n; ++j) {
+                mini = min(mini, abs(matrix[i][j]));
+                if (matrix[i][j] < 0)
+                    neg++;
+                maxi += abs(matrix[i][j]);
             }
         }
-        if(neg%2!=0){
-            maxi -= 2*mini;
-        }
+        if (neg % 2 != 0)
+            maxi -= 2 * mini;
         return maxi;
     }
 };
