3e910800cda7938515a381e42e979494ab6d1d95
Nkosinathi Michael Sibanda <23mh1a05h9@acoe.edu.in>
2024-11-28 00:32:29 +0530

    Time: 0 ms (100.00%) | Memory: 43.5 MB (13.53%) - LeetSync

--- 954-maximum-sum-circular-subarray/maximum-sum-circular-subarray.cpp
+++ 954-maximum-sum-circular-subarray/maximum-sum-circular-subarray.cpp
@@ -5,9 +5,11 @@ public:
         int curr = 0, curr_neg = 0;
         int maxi = arr[0], mini = arr[0];
         for (int i = 0; i < arr.size(); i++) {
+            //kadane's algorithm for max
             curr = max(curr + arr[i], arr[i]);
             maxi = max(maxi, curr);
 
+            //kadane's algorithm for min
             curr_neg = min(curr_neg + arr[i], arr[i]);
             mini = min(mini, curr_neg);
 
