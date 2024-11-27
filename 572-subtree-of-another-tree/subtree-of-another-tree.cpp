1889244f467faa743f9429ad23047c8ef8f1b2b1
Nkosinathi Michael Sibanda <23mh1a05h9@acoe.edu.in>
2024-11-27 14:21:47 +0530

    Time: 6 ms (8.45%) | Memory: 32.2 MB (5.37%) - LeetSync

---
+++ 572-subtree-of-another-tree/subtree-of-another-tree.cpp
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void preorder(TreeNode* node, string &hash){
+        if(node == NULL){
+            hash += 'N';
+            return;
+        }
+        hash += "," + to_string(node->val);
+        preorder(node->left, hash);
+        preorder(node->right, hash);
+        hash += '#';
+    }
+    bool isSubtree(TreeNode* root, TreeNode* subRoot) {
+        string str1, str2;
+        preorder(root, str1);
+        preorder(subRoot, str2);
+        return str1.find(str2) != string::npos;
+    }
+};
