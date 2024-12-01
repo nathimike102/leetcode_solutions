01842ed8a27014fd080d2722c025209c1e3b97bd
Nkosinathi Michael Sibanda <23mh1a05h9@acoe.edu.in>
2024-12-01 19:50:06 +0530

    Time: 0 ms (100.00%) | Memory: 13.4 MB (24.36%) - LeetSync

--- 1468-check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.cpp
+++ 1468-check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.cpp
@@ -1,14 +1,11 @@
 class Solution {
 public:
     bool checkIfExist(vector<int>& arr) {
-        map<int, int> mpp;
-        for(int i=0;i<arr.size();i++){
-            mpp[arr[i]]++;
-        }
-        for(int i=0;i<arr.size();i++){
-            if(mpp[arr[i]*2] > 0){
-                if(arr[i] == 0 && mpp[arr[i]] == 1) continue;
-                return true;
+        for (int i = 0; i < arr.size(); i++) {
+            for (int j = i+1; j < arr.size(); j++) {
+                if (arr[i] == 2 * arr[j] || arr[i] * 2 == arr[j]) {
+                    return true;
+                }
             }
         }
         return false;
