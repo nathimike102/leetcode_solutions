2d4255210739344dc97755e7953d1a533f367f6e
Nkosinathi Michael Sibanda <23mh1a05h9@acoe.edu.in>
2025-01-29 14:36:03 +0530

    Time: 2178 ms (5.64%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1462-list-the-products-ordered-in-a-period/list-the-products-ordered-in-a-period.sql
@@ -0,0 +1,2 @@
+# Write your MySQL query statement below
+SELECT p.product_name, SUM(o.unit) as unit FROM Products p JOIN Orders o USING(product_id) WHERE o.order_date LIKE '2020-02-%' GROUP BY p.product_id HAVING SUM(o.unit) >= 100;
