19d042d262e424e7cf2cf91f62a0a478e5d433ba
Nkosinathi Michael Sibanda <23mh1a05h9@acoe.edu.in>
2025-02-26 23:01:33 +0530

    Time: 1110 ms (20.31%) | Memory: 0B (100.00%) - LeetSync

---
+++ 197-rising-temperature/rising-temperature.sql
@@ -0,0 +1,2 @@
+# Write your MySQL query statement below
+SELECT w1.id FROM Weather w1 WHERE w1.temperature > (SELECT w2.temperature FROM Weather w2 WHERE W2.recordDate = DATE_SUB(w1.recordDate, INTERVAL 1 DAY));
