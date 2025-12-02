861545e17a10fe9e1e49a4f9474499936ccb5ba1
Nkosinathi Michael Sibanda <23mh1a05h9@acoe.edu.in>
2025-12-02 15:26:20 +0530

    Time: 3 ms (86.47%) | Memory: 38.7 MB (87.86%) - LeetSync

---
+++ 1708-design-parking-system/design-parking-system.cpp
@@ -0,0 +1,23 @@
+class ParkingSystem {
+public:
+    int slots[3];
+    ParkingSystem(int big, int medium, int small) {
+        slots[0] = big;
+        slots[1] = medium;
+        slots[2] = small;
+    }
+    
+    bool addCar(int carType) {
+        if(slots[carType - 1]){
+            slots[carType - 1]--;
+            return true;
+        }
+        return false;
+    }
+};
+
+/**
+ * Your ParkingSystem object will be instantiated and called as such:
+ * ParkingSystem* obj = new ParkingSystem(big, medium, small);
+ * bool param_1 = obj->addCar(carType);
+ */
