c6016c88b6be9452f8a53435e6a539da83fe2015
Nkosinathi Michael Sibanda <23mh1a05h9@acoe.edu.in>
2025-12-09 20:09:40 +0530

    Time: 3 ms (58.58%) | Memory: 9.6 MB (72.38%) - LeetSync

--- 43-multiply-strings/multiply-strings.cpp
+++ 43-multiply-strings/multiply-strings.cpp
@@ -1,49 +1,30 @@
 class Solution {
 public:
-    string mul(int x, string num1, int n){
-        int curr = 0;
-        reverse(num1.begin(), num1.end());
-        for(int i=0;i<num1.size();i++){
-            int val = (num1[i]-'0') * x + curr;
-            num1[i] = val%10 + '0';
-            curr = val/10;
-        }
-        if(curr) num1 += (curr + '0');
-        reverse(num1.begin(), num1.end());
-        for(int i=0;i<n;i++){
-            num1+='0';
-        }
-        return num1;
-    }
-    string add(string str1, string str2){
-        int curr = 0;
-        reverse(str1.begin(), str1.end());
-        reverse(str2.begin(), str2.end());
-        int i;
-        for(i=0;i<str1.length();i++){
-            int val = (str1[i] - '0') + (str2[i] - '0') + curr;
-            str2[i] = val%10 + '0';
-            curr = val/10;
-        }
-        for(i;i<str2.length();i++){
-            int val = curr + (str2[i] - '0');
-            str2[i] = val%10 + '0';
-            curr = val/10;
-        }
-        if(curr) str2 += (curr+ '0');
-        reverse(str2.begin(), str2.end());
-        return str2;
-    }
     string multiply(string num1, string num2) {
-        string str = "";
-        int n = num2.length();
-        reverse(num2.begin(), num2.end());
-        for(int i=0;i<n;i++){
-            int x = num2[i] - '0';
-            string temp = mul(x, num1, i);
-            str = add(str, temp);
+        if (num1 == "0" || num2 == "0") return "0";
+
+        int n = num1.size();
+        int m = num2.size();
+
+        vector<int> result(n + m, 0);
+
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = m - 1; j >= 0; j--) {
+                int product = (num1[i] - '0') * (num2[j] - '0');
+                int sum = product + result[i + j + 1];
+                result[i + j + 1] = sum % 10;
+                result[i + j] += sum / 10;
+            }
         }
-        if(str[0] == '0') str = '0';
-        return str;
+
+        string ans = "";
+        for (int x : result) {
+            if (!(ans.empty() && x == 0)) {
+                ans.push_back(x + '0');
+            }
+        }
+
+        if (ans == "") return "0";
+        return ans;
     }
 };
