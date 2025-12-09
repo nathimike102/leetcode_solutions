ce65904e4d531c993334b07a40e642eddef989e1
Nkosinathi Michael Sibanda <23mh1a05h9@acoe.edu.in>
2025-12-09 14:13:44 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (5.34%) - LeetSync

---
+++ 874-backspace-string-compare/backspace-string-compare.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    string get_min(string s){
+        stack<char> st;
+        for(char ch : s){
+            if(ch == '#'){
+                if(!st.empty()) st.pop();
+            }else{
+                st.push(ch);
+            }
+        }
+        string str;
+        while(!st.empty()){
+            str = st.top() + str;
+            st.pop();
+        }
+        return str;
+    }
+    bool backspaceCompare(string s, string t) {
+        string str1 = get_min(s), str2 = get_min(t);
+        cout<<str1<<" "<<str2;
+        return str1 == str2;
+    }
+};
