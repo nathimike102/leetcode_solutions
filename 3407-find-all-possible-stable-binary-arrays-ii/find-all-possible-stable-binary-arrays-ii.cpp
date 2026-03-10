90648ace9a8374e9a024c4a39869fdd37ba320b3
Nkosinathi Michael Sibanda <23mh1a05h9@acoe.edu.in>
2026-03-10 20:38:31 +0530

    Time: 51 ms (100.00%) | Memory: 17 MB (93.15%) - LeetSync

--- 3407-find-all-possible-stable-binary-arrays-ii/find-all-possible-stable-binary-arrays-ii.cpp
+++ 3407-find-all-possible-stable-binary-arrays-ii/find-all-possible-stable-binary-arrays-ii.cpp
@@ -1,42 +1,28 @@
 class Solution {
 public:
+    const int MOD = 1e9 + 7;
     int numberOfStableArrays(int zero, int one, int limit) {
-        const int mod = 1'000'000'007;
-        int L = limit + 1;
+        int dp[zero + 1][one + 1][2];
+        memset(dp, 0, sizeof(dp));
+        for (int i = min(zero, limit); i >= 0; i--)
+            dp[i][0][0] = 1;
+        for (int j = min(one, limit); j >= 0; j--)
+            dp[0][j][1] = 1;
+        for (int i = 1; i <= zero; i++) {
+            for (int j = 1; j <= one; j++) {
+                // append 0
+                dp[i][j][0] =
+                    (0LL + dp[i - 1][j][0] + dp[i - 1][j][1] -
+                     (i - 1 - limit >= 0 ? dp[i - 1 - limit][j][1] : 0) + MOD) %
+                    MOD;
 
-        // Swapping 0 and 1 doesn't change the count, but can reduce memory (O(one*L))
-        if (one > zero) swap(zero, one);
-
-        // First row
-        vector<int> dp0_prev(one + 1, 0), dp1_prev(one + 1, 0);
-        for (int j = 1; j <= min(one, limit); ++j) dp1_prev[j] = 1;
-
-        // Store last L = limit + 1 dp1 rows to access dp1[i-L][j]
-        deque<vector<int>> dp1q;
-        dp1q.push_back(dp1_prev);
-
-        for (int i = 1; i <= zero; ++i) {
-            vector<int> dp0(one + 1, 0), dp1(one + 1, 0);
-            if (i <= limit) 
-                dp0[0] = 1;
-
-            for (int j = 1; j <= one; ++j) {
-                dp0[j] = (dp0_prev[j] + dp1_prev[j] - (i >= L ? dp1q.front()[j] : 0)) % mod;
-                dp1[j] = (dp1[j - 1] + dp0[j - 1] - (j >= L ? dp0[j - L] : 0)) % mod;
-
-                // Fix negatives
-                dp0[j] = (dp0[j] + mod) % mod;
-                dp1[j] = (dp1[j] + mod) % mod;
+                // append 1
+                dp[i][j][1] =
+                    (0LL + dp[i][j - 1][0] + dp[i][j - 1][1] -
+                     (j - 1 - limit >= 0 ? dp[i][j - 1 - limit][0] : 0) + MOD) %
+                    MOD;
             }
-
-            dp1q.push_back(dp1);
-            if (dp1q.size() > L) 
-                dp1q.pop_front();
-
-            dp0_prev.swap(dp0);
-            dp1_prev.swap(dp1);
         }
-
-        return (dp0_prev[one] + dp1_prev[one]) % mod;
+        return (dp[zero][one][0] + dp[zero][one][1]) % MOD;
     }
 };
