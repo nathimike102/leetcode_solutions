981b5dff307c42830a2dd3b31aaf775ee73f3a1e
Nkosinathi Michael Sibanda <23mh1a05h9@acoe.edu.in>
2026-03-12 15:50:02 +0530

    Time: 18 ms (67.32%) | Memory: 17.6 MB (48.42%) - LeetSync

--- 3406-find-all-possible-stable-binary-arrays-i/find-all-possible-stable-binary-arrays-i.cpp
+++ 3406-find-all-possible-stable-binary-arrays-i/find-all-possible-stable-binary-arrays-i.cpp
@@ -2,27 +2,25 @@ class Solution {
 public:
     long long MOD = 1e9 + 7;
     int numberOfStableArrays(int zero, int one, int limit) {
-        vector<vector<vector<long long>>> dp(
-            zero + 1,
-            vector<vector<long long>>(one + 1, vector<long long>(2, 0)));
+        vector<vector<int>> dp0(zero+1, vector<int>(one+1)), dp1(zero+1, vector<int>(one+1));
         for (int i = 0; i <= min(zero, limit); i++)
-            dp[i][0][0] = 1;
+            dp0[i][0] = 1;
         for (int j = 0; j <= min(one, limit); j++)
-            dp[0][j][1] = 1;
+            dp1[0][j] = 1;
         for (int i = 1; i <= zero; i++) {
             for (int j = 1; j <= one; j++) {
                 // append 0
-                dp[i][j][0] =
-                    (0LL + dp[i - 1][j][0] + dp[i - 1][j][1] -
-                     (i - 1 - limit >= 0 ? dp[i - 1 - limit][j][1] : 0) + MOD) %
+                dp0[i][j] =
+                    (0LL + dp0[i - 1][j] + dp1[i - 1][j] -
+                     (i - 1 - limit >= 0 ? dp1[i - 1 - limit][j] : 0) + MOD) %
                     MOD;
                 // append 1
-                dp[i][j][1] =
-                    (0LL + dp[i][j - 1][0] + dp[i][j - 1][1] -
-                     (j - 1 - limit >= 0 ? dp[i][j - 1 - limit][0] : 0) + MOD) %
+                dp1[i][j] =
+                    (0LL + dp0[i][j - 1] + dp1[i][j - 1] -
+                     (j - 1 - limit >= 0 ? dp0[i][j - 1 - limit] : 0) + MOD) %
                     MOD;
             }
         }
-        return (dp[zero][one][0] + dp[zero][one][1]) % MOD;
+        return (dp0[zero][one] + dp1[zero][one]) % MOD;
     }
 };
