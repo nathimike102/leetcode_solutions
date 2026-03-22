e8a8faecae35e4c53cc5b4b98ef0e7f7bed90df9
Nkosinathi Michael Sibanda <23mh1a05h9@acoe.edu.in>
2026-03-22 17:33:33 +0530

    Time: 4 ms (20.74%) | Memory: 44.3 MB (5.23%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/product-of-array-except-self.cpp
@@ -2,7 +2,8 @@ class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
         int n = nums.size();
-        vector<long long> pref(n, 1), suf(n, 1);
+        vector<long long> pref(n), suf(n);
+        pref[0] = suf[n-1] = 1;
         for(int i=1;i<n;i++){
             pref[i] = pref[i-1] * nums[i-1];
         }
