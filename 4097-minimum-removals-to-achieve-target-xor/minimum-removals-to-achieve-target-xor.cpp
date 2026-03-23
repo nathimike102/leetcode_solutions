a4b2fb277d4e1e9d96965d0ce8307ee1c9bd07de
Nkosinathi Michael Sibanda <23mh1a05h9@acoe.edu.in>
2026-03-23 15:16:24 +0530

    Time: 215 ms (64.71%) | Memory: 113.9 MB (35.29%) - LeetSync

--- 4097-minimum-removals-to-achieve-target-xor/minimum-removals-to-achieve-target-xor.cpp
+++ 4097-minimum-removals-to-achieve-target-xor/minimum-removals-to-achieve-target-xor.cpp
@@ -3,20 +3,23 @@ public:
     int minRemovals(vector<int>& nums, int target) {
         for (int x : nums)
             target ^= x;
-        
-        int maxi = 1<<14;
+
+        int maxi = 1 << 14;
         vector<int> dp(maxi, INT_MAX);
         dp[0] = 0;
-        
+
         for (int x : nums) {
             for (int i = 0; i < maxi; i++) {
                 int j = i ^ x;
-                if (i > j) continue;
-                if (dp[j] != INT_MAX) dp[i] = min(dp[i], dp[j] + 1);
-                if (dp[i] != INT_MAX) dp[j] = min(dp[j], dp[i] + 1);
+                if (i > j)
+                    continue;
+                if (dp[j] != INT_MAX)
+                    dp[i] = min(dp[i], dp[j] + 1);
+                if (dp[i] != INT_MAX)
+                    dp[j] = min(dp[j], dp[i] + 1);
             }
         }
-        
+
         return dp[target] == INT_MAX ? -1 : dp[target];
     }
 };
